d7fe9db783b166157defb5cad2cd4384cf7b0217
changlei.yin <changlei.yin@qq.com>
2021-01-21 16:43:24 +0800

    init

---
+++ ketchen/src/main/java/com/sevenk/kitchen/KitchenApplication.java
@@ -0,0 +1,15 @@
+package com.sevenk.kitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author changlei.yin
+ * @since 2021/1/21 15:07
+ */
+@SpringBootApplication
+public class KitchenApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KitchenApplication.class, args);
+    }
+}
